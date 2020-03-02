7a35e45e3e8a1ed4cf7a9e0d5960408f684891a5
nikuni095 <nikhil.uni095@gmail.com>
2020-03-02 19:53:18 +0530

    learned

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          finally
+          finally learned
         </p>
       </header>
     </div>
